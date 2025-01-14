dc2b5b7eab21f9f8c18d01604ea707d0d68a4a4c
Dolfie <35562093+njvanas@users.noreply.github.com>
2025-01-14 20:58:08 +0100

    XD bye

--- vite.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  base: '/', // For a root-level GitHub Pages deployment
-  plugins: [react()],
-})
