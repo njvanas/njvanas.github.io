ba2dc95ebf724d4c9af727d82b4819e02e4e658b
Dolfie <35562093+njvanas@users.noreply.github.com>
2025-01-24 00:09:52 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import mkcert from 'vite-plugin-mkcert';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({ // Set the base path to match the GitHub repository name
-  base: process.env.NODE_ENV === 'production' ? '/njvanas.github.io/' : '/', // Dynamic base path
+  base: process.env.NODE_ENV === 'github' ? '/njvanas.github.io/' : '/', // Dynamic base path
   plugins: [react()],
   build: {
     outDir: 'dist', // Ensure output directory is named 'dist'
