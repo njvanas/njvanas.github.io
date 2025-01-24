ef3dc8551a21f0ab95e367e5aad0b7f0b39b027a
Dolfie <35562093+njvanas@users.noreply.github.com>
2025-01-24 02:06:06 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,12 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   // Set the base path to match the GitHub repository name
-  base: '/', // Replace <repository-name> with your actual repository name
+  base: '/', // Replace with your GitHub repository name, if necessary
   plugins: [react()],
   build: {
-	rollupOptions: {
-    external: ['react-router-dom']
-	{,
+    rollupOptions: {
+      external: ['react-router-dom'], // Specify external dependencies
+    },
     outDir: 'dist', // Ensure output directory is named 'dist'
     assetsDir: 'assets', // Specify directory for assets
   },
